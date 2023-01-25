dcc089db66a93acf6eee3b33f045a47f6f7c82b6
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2023-01-25 22:12:36 +0530

    Integrate nunjucks view engine (#4)

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ dotenv.config();
 const express = require("express");
 const app = express();
 
+const nunjucks = require("nunjucks");
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+app.get("/", function (req, res) {
+  res.render("index.njk", { page: "HOME PAGE" });
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
